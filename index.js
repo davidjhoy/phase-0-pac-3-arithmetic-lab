defa83d47802e8a1eaa953ec0195727969472637
david <davidjhoyjr@gmail.com>
2022-05-05 20:59:50 -0400

    commit tree f1c56e89f1c472d59194a94532bb7c66cf3450de

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,n){
+    return a+n;
+}
+
+function subtract(a,m){
+    return a-m;
+}
+
+function multiply(a, n){
+    return a*n;
+
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function increment(n){
+    return n+= 1;
+}
+
+function decrement(n){
+    return n-=1;
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n, 10);
+}
